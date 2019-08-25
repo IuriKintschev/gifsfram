d65ebb70c2739f2ed6f7742d10edca80aebb103b
IuriKintschev <jtc.iuri07@gmail.com>
2019-08-25 11:22:58 -0400

    appbar sliver retirado

--- lib/ui/home/home_ui.dart
+++ lib/ui/home/home_ui.dart
@@ -8,67 +8,62 @@ class HomeUi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black45,
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            floating: true,
-            actions: <Widget>[
-              Align(
-                alignment: Alignment.center,
-                child: Text(
-                  "0",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                  ),
-                ),
+      appBar: AppBar(
+        backgroundColor: Color(0xff8844dd),
+        title: Container(
+          height: 30,
+          child: Image.asset("assets/giphy.png"),
+        ),
+        actions: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+              "0",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
               ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.star_half,
-                  size: 30,
-                  color: Colors.yellowAccent,
-                ),
-              ),
-              IconButton(
-                onPressed: () async {
-                  String result = await showSearch(
-                      context: context, delegate: DataSearch()); //*datasearch
-                  if (result != null) {
-                    BlocProvider.getBloc<GifsBloc>().inSearch.add(result);
-                  }
-                },
-                icon: Icon(
-                  Icons.search,
-                  size: 30,
-                  color: Colors.white,
-                ),
-              )
-            ],
-            backgroundColor: Color(0xff8844dd),
-            title: Container(
-              height: 30,
-              child: Image.asset("assets/giphy.png"),
             ),
           ),
-          StreamBuilder(
-            stream: BlocProvider.getBloc<GifsBloc>().outGifs,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return ListView.builder(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (context, index) {
-                    return GifsTile(snapshot.data[index]);
-                  },
-                );
-              } else {
-                return Container();
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.star_half,
+              size: 30,
+              color: Colors.yellowAccent,
+            ),
+          ),
+          IconButton(
+            onPressed: () async {
+              String result = await showSearch(
+                  context: context, delegate: DataSearch()); //*datasearch
+              if (result != null) {
+                BlocProvider.getBloc<GifsBloc>().inSearch.add(result);
               }
             },
-          ),
+            icon: Icon(
+              Icons.search,
+              size: 30,
+              color: Colors.white,
+            ),
+          )
         ],
+      ),
+      backgroundColor: Colors.black45,
+      body: StreamBuilder(
+        stream: BlocProvider.getBloc<GifsBloc>().outGifs,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, index) {
+                return GifsTile(snapshot.data[index]);
+              },
+            );
+          } else {
+            return Container();
+          }
+        },
       ),
     );
   }
--- lib/ui/home/widgets/gif_tile.dart
+++ lib/ui/home/widgets/gif_tile.dart
@@ -8,6 +8,37 @@ class GifsTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 16.0 / 9.0,
+            child: Image.network(
+              gif.imageSmall,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(8, 8, 8, 0),
+                      child: Text(
+                        gif.title,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
