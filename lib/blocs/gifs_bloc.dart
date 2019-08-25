07fc0a5cedcf1f620ed5064646f030207381001b
IuriKintschev <jtc.iuri07@gmail.com>
2019-08-24 23:57:24 -0400

    contollers get set

--- lib/blocs/gifs_bloc.dart
+++ lib/blocs/gifs_bloc.dart
@@ -11,23 +11,25 @@ class GifsBloc implements BlocBase {
   GifsBloc() {
     api = new Api();
 
-    _searchController.stream.listen(_search);
+    _searchController.stream.listen(_search); //! cahamando search api.dart
   }
 
-  List<Gifs> gifs;
+  List<Gifs> gifs; //! gifs
 
   final StreamController _gifsController = new StreamController();
-  Stream get outGifs => _gifsController.stream;
+  Stream get outGifs => _gifsController.stream; //! saida  de gifs
 
   final _searchController = new StreamController();
-  Sink get inSearch => _searchController.sink;
+  Sink get inSearch => _searchController.sink; //! entrada de gifs
 
+  //! recupera search da api.dart e insere outGifs
   void _search(String search) async {
     gifs = await api.searchGifs(search);
 
     print(gifs);
   }
 
+  //! finalisando streams
   @override
   void dispose() {
     //dispose
