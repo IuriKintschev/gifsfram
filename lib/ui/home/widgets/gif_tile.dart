8026a5f40cb86a3e27cbef201e1e8fa77a2ff0c6
IuriKintschev <jtc.iuri07@gmail.com>
2019-08-25 11:30:56 -0400

    listando gifs com busca

--- lib/ui/home/widgets/gif_tile.dart
+++ lib/ui/home/widgets/gif_tile.dart
@@ -10,34 +10,41 @@ class GifsTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          AspectRatio(
-            aspectRatio: 16.0 / 9.0,
-            child: Image.network(
-              gif.imageSmall,
-              fit: BoxFit.cover,
-            ),
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(8, 8, 8, 0),
-                      child: Text(
-                        gif.title,
-                      ),
-                    ),
-                  ],
-                ),
+      child: Card(
+        color: Color(0xff111111),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 16.0 / 9.0,
+              child: Image.network(
+                gif.imageSmall,
+                fit: BoxFit.cover,
               ),
-            ],
-          ),
-        ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Text(
+                          gif.title,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 17,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
